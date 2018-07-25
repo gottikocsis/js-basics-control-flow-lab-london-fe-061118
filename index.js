cb0ebe4552a8345bfef905b918a5efcc0078d312
kogotti@yahoo.com <kogotti@yahoo.com>
2018-07-25 17:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     return "This one is on me!";
   }
-  if (someValue > 2000 && someValue <= 2500) {
+  else if (someValue > 2000 && someValue <= 2500) {
     return "I will gladly take your thirty bucks.";
   }
-  if (someValue > 2500) {
+  else if (someValue > 2500) {
     return "No can do.";
   }
 }
